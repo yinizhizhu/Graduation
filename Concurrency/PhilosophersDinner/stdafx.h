2bd082636cfd30c0d72f131af0cc841306cedc33
yinizhizhu <lijiahe19941013@sina.cn>
2017-04-06 09:50:48 +0800

    Update

---
+++ Concurrency/PhilosophersDinner/stdafx.h
@@ -0,0 +1,20 @@
+// stdafx.h : 标准系统包含文件的包含文件，
+// 或是经常使用但不常更改的
+// 特定于项目的包含文件
+//
+
+#pragma once 
+#include "targetver.h" 
+#define WIN32_LEAN_AND_MEAN 
+#include <windows.h> 
+#include <commctrl.h> 
+#include <stdlib.h> 
+#include <malloc.h> 
+#include <memory.h> 
+#include <tchar.h> 
+#include <stdio.h> 
+#pragma comment ( lib, "comctl32.lib" ) 
+#pragma comment ( linker, "\"/manifestdependency:type='win32' name = 'Microsoft.Windows.Common-Controls' version = '6.0.0.0' processorArchitecture = '*' publicKeyToken = '6595b64144ccf1df' language = '*'\"")
+
+
+// TODO:  在此处引用程序需要的其他头文件
