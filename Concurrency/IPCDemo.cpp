77c23abf8c2d070f90ad12e0ecd392261492ecfa
yinizhizhu <lijiahe19941013@sina.cn>
2017-04-06 09:17:33 +0800

    Update

--- Concurrency/IPCDemo.cpp
+++ Concurrency/IPCDemo.cpp
@@ -3,13 +3,12 @@
 
 using namespace std;
 
-#define COMMUNICATION_OBJECT_NAME TEXT("_ _FILE_MAPPING_ _")
-#define SYNCHRONIZING_MUTEX_NAME TEXT( "_ _TEST_MUTEX_ _" )
-#define WINDOW_CLASS_NAME TEXT( "_ _TMPWNDCLASS_ _" )
+#define COMMUNICATION_OBJECT_NAME TEXT("__FILE_MAPPING__")
+#define SYNCHRONIZING_MUTEX_NAME TEXT( "__TEST_MUTEX__" )
+#define WINDOW_CLASS_NAME TEXT( "__TMPWNDCLASS__" )
 #define BUTTON_CLOSE 100
 
-typedef struct _tagCOMMUNICATIONOBJECT
-{
+typedef struct _tagCOMMUNICATIONOBJECT {
 	HWND hWndClient;
 	BOOL bExitLoop;
 	LONG lSleepTimeout;
@@ -20,35 +19,30 @@ HWND InitializeWnd();
 PCOMMUNICATIONOBJECT pCommObject = NULL;
 HANDLE hMapping = NULL;
 
-int main()
-{
+int main() {
 	cout << "Interprocess communication demo." << endl;
 	HWND hWnd = InitializeWnd();
-	if (!hWnd)
-	{
+	if (!hWnd) {
 		cout << "Cannot create window!" << endl << "Error:\t" <<
 			GetLastError() << endl;
 		return 1;
 	}
 	HANDLE hMutex = CreateMutex(NULL, FALSE, SYNCHRONIZING_MUTEX_NAME);
-	if (!hMutex)
-	{
+	if (!hMutex) {
 		cout << "Cannot create mutex!" << endl << "Error:\t" <<
 			GetLastError() << endl;
 		return 1;
 	}
 	hMapping = CreateFileMapping((HANDLE)-1, NULL, PAGE_READWRITE, 0,
 		sizeof(COMMUNICATIONOBJECT), COMMUNICATION_OBJECT_NAME);
-	if (!hMapping)
-	{
+	if (!hMapping) {
 		cout << "Cannot create mapping object!" << endl << "Error:\t"
 			<< GetLastError() << endl;
 		return 1;
 	}
 	pCommObject = (PCOMMUNICATIONOBJECT)MapViewOfFile(hMapping,
 		FILE_MAP_WRITE, 0, 0, 0);
-	if (pCommObject)
-	{
+	if (pCommObject) {
 		pCommObject->bExitLoop = FALSE;
 		pCommObject->hWndClient = hWnd;
 		pCommObject->lSleepTimeout = 250;
@@ -57,30 +51,26 @@ int main()
 
 	STARTUPINFO startupInfoRed = { 0 };
 	PROCESS_INFORMATION processInformationRed = { 0 };
-	STARTUPINFO startupInfoBlue = { 0 };
-	PROCESS_INFORMATION processInformationBlue = { 0 };
-
 	BOOL bSuccess = CreateProcess(TEXT("..\\Debug\\IPCWorker.exe"),
 		TEXT("red"), NULL, NULL, FALSE, 0, NULL, NULL, &startupInfoRed,
 		&processInformationRed);
-	if (!bSuccess)
-	{
+	if (!bSuccess) {
 		cout << "Cannot create process red!" << endl << "Error:\t" <<
 			GetLastError() << endl;
 		return 1;
 	}
+	STARTUPINFO startupInfoBlue = { 0 };
+	PROCESS_INFORMATION processInformationBlue = { 0 };
 	bSuccess = CreateProcess(TEXT("..\\Debug\\IPCWorker.exe"),
 		TEXT("blue"), NULL, NULL, FALSE, 0, NULL, NULL, &startupInfoBlue,
 		&processInformationBlue);
-	if (!bSuccess)
-	{
+	if (!bSuccess) {
 		cout << "Cannot create process blue!" << endl << "Error:\t" <<
 			GetLastError() << endl;
 		return 1;
 	}
 	MSG msg = { 0 };
-	while (GetMessage(&msg, NULL, 0, 0))
-	{
+	while (GetMessage(&msg, NULL, 0, 0)) {
 		TranslateMessage(&msg);
 		DispatchMessage(&msg);
 	}
@@ -91,16 +81,11 @@ int main()
 	return 0;
 }
 
-LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
-{
-	switch (uMsg)
-	{
-	case WM_COMMAND:
-	{
-		switch (LOWORD(wParam))
-		{
-		case BUTTON_CLOSE:
-		{
+LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+	switch (uMsg) {
+	case WM_COMMAND: {
+		switch (LOWORD(wParam)) {
+		case BUTTON_CLOSE: {
 			PostMessage(hWnd, WM_CLOSE, 0, 0);
 			break;
 		}
@@ -111,24 +96,19 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam)
 	{
 		pCommObject = (PCOMMUNICATIONOBJECT)MapViewOfFile(hMapping,
 			FILE_MAP_WRITE, 0, 0, 0);
-		if (pCommObject)
-		{
+		if (pCommObject) {
 			pCommObject->bExitLoop = TRUE;
 			UnmapViewOfFile(pCommObject);
 		}
 		PostQuitMessage(0);
 		break;
 	}
-	default:
-	{
-		return DefWindowProc(hWnd, uMsg, wParam, lParam);
-	}
+	default: return DefWindowProc(hWnd, uMsg, wParam, lParam);
 	}
 	return 0;
 }
 
-HWND InitializeWnd()
-{
+HWND InitializeWnd() {
 	WNDCLASSEX wndEx;
 	wndEx.cbSize = sizeof(WNDCLASSEX);
 	wndEx.style = CS_HREDRAW | CS_VREDRAW;
@@ -142,18 +122,12 @@ HWND InitializeWnd()
 	wndEx.hCursor = LoadCursor(NULL, IDC_ARROW);
 	wndEx.hIcon = LoadIcon(wndEx.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
 	wndEx.hIconSm = LoadIcon(wndEx.hInstance, MAKEINTRESOURCE(IDI_APPLICATION));
-	if (!RegisterClassEx(&wndEx))
-	{
-		return NULL;
-	}
+	if (!RegisterClassEx(&wndEx)) return NULL;
 	HWND hWnd = CreateWindow(wndEx.lpszClassName,
 		TEXT("Interprocess communication Demo"),
 		WS_OVERLAPPEDWINDOW, 200, 200, 400, 300, NULL, NULL,
 		wndEx.hInstance, NULL);
-	if (!hWnd)
-	{
-		return NULL;
-	}
+	if (!hWnd) return NULL;
 	HWND hButton = CreateWindow(TEXT("BUTTON"), TEXT("Close"),
 		WS_CHILD | WS_VISIBLE | BS_PUSHBUTTON | WS_TABSTOP,
 		275, 225, 100, 25, hWnd, (HMENU)BUTTON_CLOSE, wndEx.hInstance,
@@ -162,6 +136,5 @@ HWND InitializeWnd()
 		WS_VISIBLE, 10, 10, 365, 205, hWnd, NULL, wndEx.hInstance, NULL);
 	ShowWindow(hWnd, SW_SHOW);
 	UpdateWindow(hWnd);
-
 	return hStatic;
 }
--- Concurrency/IPCWorker.cpp
+++ Concurrency/IPCWorker.cpp
@@ -1,60 +1,46 @@
 #include <Windows.h>
+#include <stdio.h>
+#include <string.h>
 
 #define COMMUNICATION_OBJECT_NAME TEXT("__FILE_MAPPING__")
 #define SYNCHRONIZING_MUTEX_NAME TEXT( "__TEST_MUTEX__" )
 
-typedef struct _tagCOMMUNICATIONOBJECT
-{
+typedef struct _tagCOMMUNICATIONOBJECT {
 	HWND hWndClient;
 	BOOL bExitLoop;
 	LONG lSleepTimeout;
 } COMMUNICATIONOBJECT, *PCOMMUNICATIONOBJECT;
 
-int main()
-{
+int main(int argc, char* argv[]) {
 	HBRUSH hBrush = NULL;
-	hBrush = CreateSolidBrush(RGB(255, 0, 0));
-	HWND hWnd = NULL;
-	HDC hDC = NULL;
-	RECT rectClient = { 0 };
-	LONG lWaitTimeout = 0;
-	HANDLE hMapping = NULL;
-	PCOMMUNICATIONOBJECT pCommObject = NULL;
+	printf("%s\n", argv[0]);
+	if (strcmp("blue", argv[0]) == 0)	hBrush = CreateSolidBrush(RGB(0, 0, 255));//blue
+	else								hBrush = CreateSolidBrush(RGB(255, 0, 0));//red
 	BOOL bContinueLoop = TRUE;
-
 	HANDLE hMutex = OpenMutex(MUTEX_ALL_ACCESS, FALSE, SYNCHRONIZING_MUTEX_NAME);
-	hMapping = OpenFileMapping(FILE_MAP_READ, FALSE, COMMUNICATION_OBJECT_NAME);
-
-	if (hMapping)
-	{
-		while (bContinueLoop)
-		{
+	HANDLE hMapping = OpenFileMapping(FILE_MAP_READ, FALSE, COMMUNICATION_OBJECT_NAME);
+	if (hMapping) {
+		while (bContinueLoop) {
 			WaitForSingleObject(hMutex, INFINITE);
-			pCommObject = (PCOMMUNICATIONOBJECT)MapViewOfFile(hMapping,
+			PCOMMUNICATIONOBJECT pCommObject = (PCOMMUNICATIONOBJECT)MapViewOfFile(hMapping,
 				FILE_MAP_READ, 0, 0, sizeof(COMMUNICATIONOBJECT));
-
-			if (pCommObject)
-			{
+			if (pCommObject) {
+				LONG lWaitTimeout = pCommObject->lSleepTimeout;
 				bContinueLoop = !pCommObject->bExitLoop;
-				hWnd = pCommObject->hWndClient;
-				lWaitTimeout = pCommObject->lSleepTimeout;
+				HWND hWnd = pCommObject->hWndClient;
 				UnmapViewOfFile(pCommObject);
-				hDC = GetDC(hWnd);
+				HDC hDC = GetDC(hWnd);
+				RECT rectClient = { 0 };
 				if (GetClientRect(hWnd, &rectClient))
-				{
 					FillRect(hDC, &rectClient, hBrush);
-				}
-
 				ReleaseDC(hWnd, hDC);
 				Sleep(lWaitTimeout);
 			}
 			ReleaseMutex(hMutex);
 		}
 	}
-
 	CloseHandle(hMapping);
 	CloseHandle(hMutex);
 	DeleteObject(hBrush);
-
 	return 0;
 }
