626ef2a5bc41ad9974f2dfb0f4430ef6345f3617
yinizhizhu <lijiahe19941013@sina.cn>
2017-04-21 08:44:25 +0800

    update

--- Cpp-Concurrency-in-Action-master/listing_3.1.cpp
+++ Cpp-Concurrency-in-Action-master/listing_3.1.cpp
@@ -23,3 +23,25 @@ int main() {
 	add_to_list(42);
 	cout << "contains(1)=" << list_contains(1) << ", contains(42)=" << list_contains(42) << endl;
 }
+
+#include <thread>
+#include <mutex>
+#include <iostream>
+
+int g_i = 0;
+std::mutex g_i_mutex;  // protects g_i
+void safe_increment() {
+	std::lock_guard<std::mutex> lock(g_i_mutex);
+	std::cout << std::this_thread::get_id() << ": ";
+	std::cout << g_i << ", ";
+	++g_i;
+	std::cout << g_i << "\n";
+	// g_i_mutex is automatically released when lock
+	// goes out of scope
+}
+int main() {
+	std::thread t1(safe_increment);
+	std::thread t2(safe_increment);
+	t1.join();
+	t2.join();
+}
