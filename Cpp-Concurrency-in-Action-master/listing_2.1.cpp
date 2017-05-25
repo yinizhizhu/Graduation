989628437defb0c009f2fe38e080d12bc44a4190
yinizhizhu <lijiahe19941013@sina.cn>
2017-05-25 12:31:55 +0800

    Update

--- Cpp-Concurrency-in-Action-master/listing_2.1.cpp
+++ Cpp-Concurrency-in-Action-master/listing_2.1.cpp
@@ -28,3 +28,41 @@ void oops() {
 int main() {
 	oops();
 }
+
+
+
+#include <thread>
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct func
+{
+
+	func(){}
+	void operator()()
+	{
+		ofstream out("hehe.txt");
+		for (unsigned j = 0; j<100; ++j)
+		{
+			out << j << "\n";
+		}
+		out.close();
+	}
+};
+
+
+void oops()
+{
+	func my_func;
+	thread my_thread(my_func);
+	my_thread.detach();
+}
+
+int main()
+{
+	oops();
+	for (int i = 0; i < 100000000; i++);
+}
+
